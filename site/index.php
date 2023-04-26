2522be378fb36a22b309b4d52d0945bafdb41f0b
Saabbass <178414@novacollege.nl>
2023-04-26 09:52:01 +0200

    zoeken naar hero

--- site/index.php
+++ site/index.php
@@ -30,28 +30,49 @@ $all_heroes = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
     <title>superheroes</title>
 </head>
 
 <body>
 
     <!-- Hier komen alle superheroes -->
-    <?php foreach ($all_heroes as $user) : ?>
-        <p><?php echo $user["ID"] ?></p>
-        <p><?php echo $user["Title"] ?></p>
-        <p><?php echo $user["Alignment"] ?></p>
-        <p><?php echo $user["Gender"] ?></p>
-        <p><?php echo $user["Height"] ?></p>
-        <p><?php echo $user["Weight"] ?></p>
-        <p><?php echo $user["Identity"] ?></p>
-        <p><?php echo $user["MaritalStatus"] ?></p>
-        <p><?php echo $user["Eyes"] ?></p>
-        <p><?php echo $user["Hair"] ?></p>
-        <p><?php echo $user["PlaceOfBirth"] ?></p>
-        <p><?php echo $user["PlaceOfDeath"] ?></p>
-        <p><?php echo $user["Citizenship"] ?></p>
-        <p><?php echo $user["Occupation"] ?></p>
-    <?php endforeach; ?>
+        <table>
+            <thead>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Alignment</th>
+                <th>Gender</th>
+                <th>Height</th>
+                <th>Weight</th>
+                <th>Identity</th>
+                <th>MaritalStatus</th>
+                <th>Eyes</th>
+                <th>Hair</th>
+                <th>PlaceOfBirth</th>
+                <th>PlaceOfDeath</th>
+                <th>Citizenship</th>
+                <th>Occupation</th>
+            </thead>
+            <?php foreach ($all_heroes as $user) : ?>
+            <tbody>
+                <td><?php echo $user["ID"] ?></td>
+                <td><?php echo $user["Title"] ?></td>
+                <td><?php echo $user["Alignment"] ?></td>
+                <td><?php echo $user["Gender"] ?></td>
+                <td><?php echo $user["Height"] ?></td>
+                <td><?php echo $user["Weight"] ?></td>
+                <td><?php echo $user["Identity"] ?></td>
+                <td><?php echo $user["MaritalStatus"] ?></td>
+                <td><?php echo $user["Eyes"] ?></td>
+                <td><?php echo $user["Hair"] ?></td>
+                <td><?php echo $user["PlaceOfBirth"] ?></td>
+                <td><?php echo $user["PlaceOfDeath"] ?></td>
+                <td><?php echo $user["Citizenship"] ?></td>
+                <td><?php echo $user["Occupation"] ?></td>
+            </tbody>
+            <?php endforeach; ?>
+        </table>
 
 </body>
 
